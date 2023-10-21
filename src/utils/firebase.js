dd0e3a13fc466baf2ca929d596bf9bf814695257
MOHIT KUMAR <mohitkr7982@gmail.com>
2023-10-21 14:45:11 +0530

    firebase setup

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO : Add SDKs for Firebase products that you want to use
+// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
