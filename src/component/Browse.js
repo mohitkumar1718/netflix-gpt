680a13841b224d277c75942ea112ec6dae25b493
Mohit Kumar <mohitkr7982@gmail.com>
2024-01-21 16:52:10 +0530

    created a movies sclice in redux store

--- src/component/Browse.js
+++ src/component/Browse.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTIONS } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
 export const Browse = () => {
-
+    const dispatch=useDispatch();
 	const nowPlayingMovies = async ()=>{
+		
+
 		const data= await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS);
         const json= await data.json();
 		console.log(json.results);
+        dispatch(addNowPlayingMovies(json.results));
 	}
 	useEffect(()=>{
 		nowPlayingMovies();
--- src/utils/appstore.js
+++ src/utils/appstore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "./userSlice";
+import moviesReduces from "./movieSlice"
 const appStore = configureStore({
 	reducer: {
 		user: useReducer,
+		movies:moviesReduces,
 	},
 });
 export default appStore;
